e2b57faa198fbb5f0824d07a57bc712d1ab11eee
J Seymour <JustJakeSeymour@gmail.com>
2023-01-16 13:06:02 -0700

    bulk discount edit feature

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -12,11 +12,11 @@ class Merchants::BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @bulk_discount = BulkDiscount.new
   end
-
+  
   def create
     merchant = Merchant.find(params[:merchant_id])
     bulk_discount = merchant.bulk_discounts.new(bulk_discount_params)
-
+    
     if bulk_discount.save
       redirect_to merchant_bulk_discounts_path(params[:merchant_id])
       flash[:alert] = "Bulk discount created."
@@ -25,17 +25,30 @@ class Merchants::BulkDiscountsController < ApplicationController
       render :new
     end
   end
-
+  
   def edit
-    
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
+  
+  def update
+    bulk_discount = BulkDiscount.find(params[:id])
+
+    if bulk_discount.update!(bulk_discount_params)
+      redirect_to merchant_bulk_discount_path(bulk_discount.merchant_id, bulk_discount.id)
+      flash[:alert] = "Bulk discount created."
+    else
+      flash[:alert] = "Fields cannot be blank"
+      render :edit
+    end
   end
   
   def destroy
     BulkDiscount.delete(params[:id])
-
+    
     redirect_to merchant_bulk_discounts_path(params[:merchant_id])
   end
-
+  
   private
   def bulk_discount_params
     params.require(:bulk_discount).permit(:percent_discount, :quantity_threshold)
--- spec/features/merchants/bulk_discounts/edit_spec.rb
+++ spec/features/merchants/bulk_discounts/edit_spec.rb
@@ -2,10 +2,25 @@ require 'rails_helper'
 
 RSpec.describe "Merchant's bulk discount edit" do
   let!(:merchant) {create(:merchant)}
-  let!(:bulk_discount1) {create(:bulk_discount, merchant_id: merchant.id)}
+  let!(:bulk_discount1) {create(:bulk_discount, merchant_id: merchant.id, percent_discount: 20)}
 
   describe "user story 5 part 2" do
     it "edit form" do 
+      visit edit_merchant_bulk_discount_path(merchant.id, bulk_discount1.id)
+      
+      # And I see that the discounts current attributes are pre-poluated in the form
+      expect(page).to have_field("Percent discount", with: "20")
+      expect(page).to have_field("Quantity threshold", with: "#{bulk_discount1.quantity_threshold}")
+      
+      # When I change any/all of the information and click submit
+      fill_in("Percent discount", with: 30)
+      click_button "Update Bulk discount"
+
+      # Then I am redirected to the bulk discount's show page
+      expect(current_path).to eq(merchant_bulk_discount_path(merchant.id, bulk_discount1.id))
+
+      # And I see that the discount's attributes have been updated
+      expect(page).to have_content("Percent discount: 30%")
 
     end
   end
