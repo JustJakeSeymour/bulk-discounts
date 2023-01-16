b8621abc8e9483dc691e489a1220000859972210
J Seymour <JustJakeSeymour@gmail.com>
2023-01-16 11:17:59 -0700

    bulk discount create

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -2,10 +2,31 @@ class Merchants::BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
   end
-
+  
   def show
   end
-
+  
   def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.new
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = merchant.bulk_discounts.new(bulk_discount_params)
+
+    if bulk_discount.save
+      redirect_to merchant_bulk_discounts_path(params[:merchant_id])
+      flash[:alert] = "Bulk discount created."
+    else
+      flash[:alert] = "Fields cannot be blank"
+      render :new
+    end
+    
+  end
+  
+  private
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:percent_discount, :quantity_threshold)
   end
 end
--- spec/features/merchants/bulk_discounts/create_spec.rb
+++ spec/features/merchants/bulk_discounts/create_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Merchant Bulk Discount Create" do
+  let!(:merchant) {create(:merchant)}
+
   describe "user story 2, continued" do
-    xit "form to add new bulk discount" do
+    it "form to add new bulk discount" do
       visit new_merchant_bulk_discount_path(merchant.id)
 
-      expect(page).to have_form("New")
+      expect(page).to have_content("Percent discount")
+
+      fill_in("Percent discount", with: 20)
+      fill_in("Quantity threshold", with: 5)
+      click_button("Create Bulk discount")
+
+      expect(current_path).to eq(merchant_bulk_discounts_path(merchant.id))
+
+      
+      expect(page).to have_content("20")
+      expect(page).to have_content("5")
     end
   end
 end
