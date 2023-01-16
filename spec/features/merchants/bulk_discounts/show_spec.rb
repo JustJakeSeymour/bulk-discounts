087c8279cd1d25875ad06ccad60002874ae12331
J Seymour <JustJakeSeymour@gmail.com>
2023-01-16 11:45:35 -0700

    begin show page

--- spec/features/merchants/bulk_discounts/show_spec.rb
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Merchant Bulk Discount Show" do
+  let!(:merchant) {create(:merchant)}
+  let!(:bulk_discount1) {create(:bulk_discount, merchant_id: merchant.id)}
+
   describe "user story 4" do
     it "displays a bulk discount information belonging to merchant" do
       visit merchant_bulk_discount_path(merchant.id, bulk_discount1.id)
